c01cd4b8cacd575a8b8f8033099e8180cca23a7b
Jakob Linscheid <ultimatedark54@gmail.com>
2025-10-07 11:28:19 -0500

    work october 7th

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,36 @@
 package echoserver;
 
 public class EchoClient {
+    
+    public static final int portNumber = 4200;
+    
+   public static void main(String[] args) throws IOException {
+    String server;
+    // Use "127.0.0.1", i.e., localhost, if no server is specified.
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+    try {
+      // Connect to the server
+      Socket socket = new Socket(server, portNumber);
+
+      // Get the input stream so we can read from that socket
+      InputStream input = socket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+      // Print all the input we receive from the server
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+
+      // Close the socket when we're done reading from it
+      socket.close();
+
+    // Provide some minimal error handling.
+    } 
+  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,34 @@
 package echoserver;
 
-public class EchoServer {
+import java.net.*;
+import java.io.*;
+
+public class DateServer {
+  public static final int portNumber = 4200;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = sock.accept();
+        System.out.println("Got a request!");
+
+        // Construct a writer so we can write to the socket, thereby
+        // sending something back to the client.
+        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+        // Send the current date back tothe client.
+        writer.println("b");
+        // writer.println(new java.util.Date().toString());
+
+        // Close the client socket since we're done.
+        client.close();
+      }
+    // *Very* minimal error handling.
+    } 
+  }
 }
